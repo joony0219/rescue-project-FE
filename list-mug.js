b55040d50feec773a299a3e7f80ab0282f87be63
[SW4/2반] 오창현 <ckdgus5189@gmail.com>
2023-04-19 00:54:05 +0900

    list-mug.js 에 JSON 데이터를 받아와 JSON 데이터 갯수만큼 상품 리스트를 만들어주는 코드 생성

---
+++ list-mug.js
@@ -0,0 +1,52 @@
+// JSON 데이터 배열 임시 생성.
+const jsonData = [
+    {
+        imgSrc: '이미지 URL 1',
+        title: '제목 1',
+        description: '설명 1'
+    },
+    {
+        imgSrc: '이미지 URL 2',
+        title: '제목 2',
+        description: '설명 2'
+    },
+    {
+        imgSrc: '이미지 URL 3',
+        title: '제목 3',
+        description: '설명 3'
+    },
+];
+
+// <div class="item-Area"> 요소 선택.
+const itemArea = document.querySelector('.item-area');
+
+// jsonData 배열 각 항목을 반복하고 <a> 요소 생성.
+const links = jsonData.map(data => {
+    // <a> 요소 생성.
+    const link = document.createElement('a');
+    link.setAttribute('href', '#');
+
+    // <img> 요소 생성하고 src와 alt 속성 설정.
+    const img = document.createElement('img');
+    img.setAttribute('src', data.imgSrc);
+    img.setAttribute('alt', '대체 텍스트');
+
+    // <h5> 요소 생성하고 텍스트 컨텐츠 설정.
+    const heading = document.createElement('h5');
+    heading.textContent = data.title;
+
+    // <p> 요소 생성하고 텍스트 컨텐츠 설정.
+    const paragraph = document.createElement('p');
+    paragraph.textContent = data.description;
+
+    // <a> 요소 하위 요소로 <img>, <h5>, <p> 요소 추가.
+    link.appendChild(img);
+    link.appendChild(heading);
+    link.appendChild(paragraph);
+
+    // 생성된 <a> 요소를 반환.
+    return link;
+});
+
+// 반환된 <a> 요소들을 <div class="item-Area">에 추가.
+links.forEach(link => itemArea.appendChild(link));
