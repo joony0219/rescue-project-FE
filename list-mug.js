a1c46675127dc974330f0c7508930725167ea60d
[SW4/2반] 오창현 <ckdgus5189@gmail.com>
2023-04-19 14:17:03 +0900

    item-area 내부에 js로 구현한 html에 css 추가

--- list-mug.js
+++ list-mug.js
@@ -1,44 +1,144 @@
 // JSON 데이터 배열 임시 생성.
 const jsonData = [
     {
+        pageLink: '#',
         imgSrc: '이미지 URL 1',
-        title: '제목 1',
-        description: '설명 1'
+        title: '킨토 캐스트 에스프레소컵 90ml',
+        description: '7,400원(부가세포함)'
     },
     {
+        pageLink: '#',
         imgSrc: '이미지 URL 2',
-        title: '제목 2',
-        description: '설명 2'
+        title: '킨토 캐스트 커피컵 220ml',
+        description: '9,900원(부가세포함)'
     },
     {
+        pageLink: '#',
         imgSrc: '이미지 URL 3',
-        title: '제목 3',
-        description: '설명 3'
+        title: '킨토 캐스트 티컵 220ml',
+        description: '12,000원(부가세포함)'
     },
+    {
+        pageLink: '#',
+        imgSrc: '이미지 URL 3',
+        title: '킨토 캐스트 티컵 220ml',
+        description: '12,000원(부가세포함)'
+    },
+    {
+        pageLink: '#',
+        imgSrc: '이미지 URL 3',
+        title: '킨토 캐스트 티컵 220ml',
+        description: '12,000원(부가세포함)'
+    },
+    {
+        pageLink: '#',
+        imgSrc: '이미지 URL 3',
+        title: '킨토 캐스트 티컵 220ml',
+        description: '12,000원(부가세포함)'
+    },
+    {
+        pageLink: '#',
+        imgSrc: '이미지 URL 3',
+        title: '킨토 캐스트 티컵 220ml',
+        description: '12,000원(부가세포함)'
+    },
+    {
+        pageLink: '#',
+        imgSrc: '이미지 URL 3',
+        title: '킨토 캐스트 티컵 220ml',
+        description: '12,000원(부가세포함)'
+    },
+    {
+        pageLink: '#',
+        imgSrc: '이미지 URL 3',
+        title: '킨토 캐스트 티컵 220ml',
+        description: '13,000원(부가세포함)'
+    },
+    {
+        pageLink: '#',
+        imgSrc: '이미지 URL 3',
+        title: '킨토 캐스트 티컵 220ml',
+        description: '13,000원(부가세포함)'
+    },
+    {
+        pageLink: '#',
+        imgSrc: '이미지 URL 3',
+        title: '킨토 캐스트 티컵 220ml',
+        description: '13,000원(부가세포함)'
+    },
+    {
+        pageLink: '#',
+        imgSrc: '이미지 URL 3',
+        title: '킨토 캐스트 티컵 220ml',
+        description: '13,000원(부가세포함)'
+    },
+    {
+        pageLink: '#',
+        imgSrc: '이미지 URL 3',
+        title: '킨토 캐스트 티컵 220ml',
+        description: '13,000원(부가세포함)'
+    },
+    {
+        pageLink: '#',
+        imgSrc: '이미지 URL 3',
+        title: '킨토 캐스트 티컵 220ml',
+        description: '13,000원(부가세포함)'
+    },
+    {
+        pageLink: '#',
+        imgSrc: '이미지 URL 3',
+        title: '킨토 캐스트 티컵 220ml',
+        description: '13,000원(부가세포함)'
+    },
+    {
+        pageLink: '#',
+        imgSrc: '이미지 URL 3',
+        title: '킨토 캐스트 티컵 220ml',
+        description: '13,000원(부가세포함)'
+    },
+    {
+        pageLink: '#',
+        imgSrc: '이미지 URL 3',
+        title: '킨토 캐스트 티컵 220ml',
+        description: '13,000원(부가세포함)'
+    },
+    {
+        pageLink: '#',
+        imgSrc: '이미지 URL 3',
+        title: '킨토 캐스트 티컵 220ml',
+        description: '13,000원(부가세포함)'
+    },
+
 ];
 
-// <div class="item-Area"> 요소 선택.
 const itemArea = document.querySelector('.item-area');
 const itemCount = document.querySelector('.item-count');
 
+//item-area에 html코드를 생성해주고 데이터와 넣어주는 코드 작성
+
 // jsonData 배열 각 항목을 반복하고 <a> 요소 생성.
 const links = jsonData.map(data => {
-    // <a> 요소 생성.
-    const link = document.createElement('a');
-    link.setAttribute('href', '#');
 
-    // <img> 요소 생성하고 src와 alt 속성 설정.
+    // <a> 요소 생성, css 조작을 위해 classList 추가.
+    const link = document.createElement('a');
+    link.setAttribute('href', jsonData.pageLink);
+    link.classList.add('item-box');
+
+    // <img> 요소 생성하고 src와 alt 속성 설정, css 조작을 위해 classList 추가.
     const img = document.createElement('img');
     img.setAttribute('src', data.imgSrc);
-    img.setAttribute('alt', '대체 텍스트');
+    img.setAttribute('alt', '이미지가 손상되었습니다');
+    img.classList.add('item-image');
 
-    // <h5> 요소 생성하고 텍스트 컨텐츠 설정.
+    // <h5> 요소 생성하고 텍스트 컨텐츠 설정, css 조작을 위해 classList 추가.
     const heading = document.createElement('h5');
     heading.textContent = data.title;
+    heading.classList.add('item-name');
 
-    // <p> 요소 생성하고 텍스트 컨텐츠 설정.
+    // <p> 요소 생성하고 텍스트 컨텐츠 설정, css 조작을 위해 classList 추가.
     const paragraph = document.createElement('p');
     paragraph.textContent = data.description;
+    paragraph.classList.add('item-price');
 
     // <a> 요소 하위 요소로 <img>, <h5>, <p> 요소 추가.
     link.appendChild(img);
@@ -54,3 +154,4 @@ links.forEach(link => itemArea.appendChild(link));
 
 //jsonData의 length를 측정하여 itemCount의 innerHTML에 추가.
 itemCount.innerHTML = `${jsonData.length}`;
+
