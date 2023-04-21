5d77c3ef179479d1beaca72fd234e109e45869bd
[SW4/2반] 오창현 <ckdgus5189@gmail.com>
2023-04-22 00:19:25 +0900

    test: fetch로 json 데이터 받아오는 로직 테스트중

--- list-detail/list-detail.js
+++ list-detail/list-detail.js
@@ -1,7 +1,34 @@
-import { jsonData } from "../list-mug/list-mug-json";
+const productName = document.querySelector('.product-name');
+const productPrice = document.querySelector('.product-price');
 
-const leftContent = document.querySelector('.left-content');
+const specificationsContainer = document.querySelector('.specifications-container');
+const handlingPrecautionsContainer = document.querySelector('.handling-precautions-container');
 
-const links = jsonData.map(data => {
+const innerContentsText1 = document.querySelector(".inner-contents1");
+const innerContentsText2 = document.querySelector(".inner-contents2");
 
-})
+fetch('../list-mug/list-mug-json.js')
+    .then(response => response.json())
+    .then(data => {
+        productName.innerHTML = data.title;
+        productPrice.innerHTML = data.description;
+        innerContentsText1.innerHTML = data.innerData.specifications;
+        innerContentsText2.innerHTML = data.innerData.handlingPrecautions;
+    })
+    .catch(error => console.log(error));
+
+specificationsContainer.addEventListener('click', function () {
+    if (innerContentsText1.style.display === "none") {
+        innerContentsText1.style.display = "block";
+    } else {
+        innerContentsText1.style.display = "none";
+    }
+});
+
+handlingPrecautionsContainer.addEventListener('click', function () {
+    if (innerContentsText2.style.display === "none") {
+        innerContentsText2.style.display = "block";
+    } else {
+        innerContentsText2.style.display = "none";
+    }
+});
