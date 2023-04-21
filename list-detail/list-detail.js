fdbc950ced8ae245c1fcec53f0015337864896c9
[SW4/2반] 오창현 <ckdgus5189@gmail.com>
2023-04-21 18:47:58 +0900

    feat: list-detail.js 추가,css 수정

--- list-detail/list-detail.js
+++ list-detail/list-detail.js
@@ -1,7 +1,33 @@
 import { jsonData } from "../list-mug/list-mug-json";
+import { innerData } from "../list-mug/list-mug-json";
 
-const leftContent = document.querySelector('.left-content');
+const productName = document.querySelector('.product-name');
+const productPrice = document.querySelector('.product-price');
 
-const links = jsonData.map(data => {
+const specificationsContainer = document.querySelector('.specifications-container');
+const handlingPrecautionsContainer = document.querySelector('.handling-precautions-container');
 
-})
+const innerContentsText1 = document.querySelector(".inner-contents1");
+const innerContentsText2 = document.querySelector(".inner-contents2");
+
+productName.innerHTML = jsonData.title;
+productPrice.innerHTML = jsonData.description;
+
+innerContentsText1.innerHTML = innerData.specifications;
+innerContentsText2.innerHTML = innerData.handlingPrecautions;
+
+
+specificationsContainer.addEventListener('click', function () {
+    if (innerContentsText1.style.display === "none") {
+        innerContentsText1.style.display = "block";
+    } else {
+        innerContentsText1.style.display = "none";
+    }
+});
+handlingPrecautionsContainer.addEventListener('click', function () {
+    if (innerContentsText2.style.display === "none") {
+        innerContentsText2.style.display = "block";
+    } else {
+        innerContentsText2.style.display = "none";
+    }
+});
