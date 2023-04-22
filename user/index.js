d4fd4a96b3528f03173a9e050f27244185fd310e
[SW4/2반] 윤성준 <tjd1wns15@gmail.com>
2023-04-22 18:02:42 +0900

    feat : 유저정보를 동적으로 추가하기 위한 코드 변경

--- user/index.js
+++ user/index.js
@@ -1,13 +1,39 @@
 async function getData() {
     try {
-        const response1 = await fetch('url1');
-        const data1 = await response1.json();
-        document.getElementById('userId').textContent = data1.userId;
-        document.getElementById('name').textContent = data1.name;
-        document.getElementById('email').textContent = data1.email;
-        document.getElementById('phoneNumber').textContent = data1.phone;
-        document.getElementById('address').textContent = data1.address;
-    
+        // 임의의 유저 데이터인 userInfo를 가져옴. 이후 서버 데이터로 변경 필요!!
+        const response = await fetch('./userInfo.json'); 
+        const userInfoData = await response.json();
+        
+        document.querySelector('.userProfile-userName').textContent = userInfoData.userId;
+        // 가져온 데이터를 테이블로 만듦
+        const userInfoTable = `
+        <div class ="userProfile-userInfo">
+            <table>
+                <tbody>
+                    <tr>
+                        <td class="tableList">이름</td>
+                        <td id="name">${userInfoData.name}</td>
+                    </tr>
+                    <tr>
+                        <td class="tableList">이메일</td>
+                        <td id="email">${userInfoData.email}</td>
+                    </tr>
+                    <tr>
+                        <td class="tableList">핸드폰 번호</td>
+                        <td id="phoneNumber">${userInfoData.phoneNumber}</td>
+                    </tr>
+                    <tr>
+                        <td class="tableList">주소</td>
+                        <td id="address">${userInfoData.address}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        `;
+
+        // insertAdjacentHTML로 전달
+        document.querySelector('.userProfile').insertAdjacentHTML('beforeend', userInfoTable);
+
         const response2 = await fetch('url2');
         const data2 = await response2.json();
         document.getElementById('orderNumber').textContent = data2.orderNumber;
