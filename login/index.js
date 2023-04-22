d9a7f7b095e0d76e6f11440bd149b56cbfda5e8e
[SW4/2반] 윤성준 <tjd1wns15@gmail.com>
2023-04-22 17:06:53 +0900

    refactor : 사용하지 않는 변수 삭제

--- login/index.js
+++ login/index.js
@@ -1,36 +1,33 @@
 // html 요소
-const idInput = document.querySelector('#idInput')
-const passwordInput = document.querySelector('#passwordInput')
+const idInput = document.querySelector('#idInput');
+const passwordInput = document.querySelector('#passwordInput');
 
-const loginButton = document.querySelector('#loginButton')
+const loginButton = document.querySelector('#loginButton');
 
 // 로그인 버튼 이벤트
-loginButton.addEventListener('click', handleSubmit)
+loginButton.addEventListener('click', handleSubmit);
 
 // 이벤트 함수
 async function handleSubmit(e) {
-  e.preventDefault()
+  e.preventDefault();
   
-  const userName = idInput.value
-  const password = passwordInput.value
-
   // 입력 여부 확인
   if (!userName) {
-    return alert("아이디를 입력해 주세요.")
+    return alert("아이디를 입력해 주세요.");
   }
   
   if (!password) {
-    return alert("비밀번호를 입력해 주세요.")
+    return alert("비밀번호를 입력해 주세요.");
   }
 
   // 객체 생성
   const data = {
-    userName,
-    password
+    userName: idInput.value,
+    password: passwordInput.value
   }
   
   // JSON 생성
-  const dataJson = JSON.stringify(data)
+  const dataJson = JSON.stringify(data);
   
   // 로그인 요청 서버에 보내기
   const response = await fetch(`/auth/login`, {
@@ -43,11 +40,11 @@ async function handleSubmit(e) {
   .then(response => response.json())
   .then(data => {
     if (res.status === 200) {
-      alert("로그인에 성공하였습니다!")
+      alert("로그인에 성공하였습니다!");
       // 로그인 후 페이지 이동
       window.location.href = '' // 메인 페이지 연결하기
     } else {
-      throw new Error('로그인 실패')
+      throw new Error('로그인 실패');
     }
   })
   .catch(error => {
