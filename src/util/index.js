9fed25c1e1e02e9315bee0343852c0548ac44de1
Baecheol <bbaecheol@gmail.com>
2023-04-26 16:06:29 +0900

    index.js 페이지 링크 되도록 수정 중

--- src/util/index.js
+++ src/util/index.js
@@ -66,42 +66,6 @@ export const drawNavbar = () => {
   // 첫 번째 header 태그를 찾아서 해당 태그의 시작 부분에 nav.html을 삽입
   const headerTag = document.getElementsByTagName('header')[0];
   headerTag.insertAdjacentHTML('afterbegin', template);
-
-  // 후에 관리자 계정 만들면 활성화
-  // if (sessionStorage.getItem('userId') && sessionStorage.getItem('adminToken')) {
-  //   userId = `/admin/`;
-  // }
-  // sessionStorage에서 userId를 가져와서 해당 값이 존재하는 경우 `/users/userlist/${userId}`를, 그렇지 않은 경우 `/users/login`을 userId 변수에 할당
-  // if (sessionStorage.getItem('userId')) {
-  //   userId = `/users/userlist/${sessionStorage.getItem('userId')}`;
-  // } else {
-  //   userId = `/users/login`;
-  // }
-
-  // // 현재 모듈의 URL을 기반으로 `./components/nav/nav.html` 경로에서 리소스를 가져오는 `fetch` 함수를 호출
-  // fetch(new URL('./components/nav/nav.html', import.meta.url))
-  //   // 가져온 html 코드를 text로 변환
-  //   .then((response) => response.text())
-  //   // 변환된 html 코드를 template 변수에 할당
-  //   .then((data) => {
-  //     template = data;
-  //     // 첫 번째 header 태그를 찾아서 해당 태그의 시작 부분에 nav.html을 삽입
-  //     const headerTag = document.getElementsByTagName('header')[0];
-  //     headerTag.insertAdjacentHTML('afterbegin', template);
-
-  //     // nav.css 파일을 가져옴
-  //     fetch(new URL('./components/nav/nav.css', import.meta.url))
-  //       .then((response) => response.text())
-  //       .then((css) => {
-  //         // head 태그를 가져와서 CSS 코드를 추가함
-  //         const headTag = document.getElementsByTagName('head')[0];
-  //         const styleTag = document.createElement('style');
-  //         styleTag.innerHTML = css;
-  //         headTag.appendChild(styleTag);
-  //       })
-  //       .catch((error) => console.error(error));
-  //   })
-  //   .catch((error) => console.error(error));
 };
 
 // footer.css를 template에 적용하는 함수
@@ -156,12 +120,52 @@ export const activeNavbar = () => {
   const userinfoBtn = document.getElementById('userinfo');
   const cartBtn = document.getElementById('cart');
 
+  // 서버 포트 URI
+  const URI = 'http://34.64.252.224';
+
+  loginBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location.href = '../login/login.html';
+  });
+
+  // userinfoBtn 클릭 시 실행되는 함수
+  userinfoBtn.addEventListener('click', async (e) => {
+    e.preventDefault();
+
+    // 서버로 signed-in 요청 보내기
+    const response = await fetch(`${URI}/api/auth/signed-in`, {
+        method: 'GET',
+        credentials: 'include',
+      })
+      .then((response) => {
+        if (response.ok) {
+          // 로그인 되었다면 바로 유저 정보 페이지로 이동
+          window.location.href = '../user/user.html';
+        } else {
+          window.location.href = '../login/login.html';
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  });
+
+  cartBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location.href = '../shopping-cart/shopping-cart.html';
+  });
+
+  logoutBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    //
+  });
+
   // userinfoBtn을 클릭했을 때 실행되는 함수
   function handleUserInfoBtnClick() {
     // jwt 토큰을 가져옴
     const token = getToken();
     // fetch를 이용해 서버에 /api/user 요청을 보냄
-    fetch('/api/user', {
+    fetch('/api/auth/user', {
       headers: {
         Authorization: `Bearer ${token}`,
       },
@@ -183,9 +187,11 @@ export const activeNavbar = () => {
       });
   }
 
-  // 로그인 여부를 확인하기 위해 세션 스토리지에서 토큰을 가져온다.
-  // 만약 로그인을 했다면, 로그아웃 버튼을 보이게 하고, 클릭 시 세션 스토리지에서 토큰을 삭제한다.
+  // 세션 스토리지에서 토큰을 가져와 로그인 여부를 확인한다.
   if (sessionStorage.getItem('loginToken')) {
+    // 만약 로그인을 했다면, 로그인 버튼을 비활성화하고 로그아웃 버튼을 활성화한다.
+    // 또한, 로그아웃 버튼 클릭 시 세션 스토리지에서 토큰과 관련된 정보를 삭제한다.
+    loginBtn.classList.add('active');
     logoutBtn.classList.remove('active');
     logoutAfter.classList.remove('active');
     logoutBtn.addEventListener('click', () => {
@@ -194,7 +200,7 @@ export const activeNavbar = () => {
       sessionStorage.removeItem('adminToken');
     });
   } else {
-    // 로그인을 하지 않았다면, 로그아웃 버튼을 숨기고, 유저 정보 버튼 클릭 시 로그인 후 이용 가능하다는 알림창을 띄운다.
+    // 로그인을 하지 않았다면, 로그아웃 버튼을 활성화하지 않고, 유저 정보 버튼 클릭 시 로그인 후 이용 가능하다는 알림창을 띄운다.
     console.log('로그인 확인 토큰 없음');
     logoutBtn.classList.add('active');
     logoutAfter.classList.add('active');
@@ -204,9 +210,9 @@ export const activeNavbar = () => {
   }
 
   // 로그아웃 시 세션스토리지 토큰 제거
-  // if (logoutBtn.classList.contains('active')) {
-  //   sessionStorage.removeItem('loginToken');
-  //   sessionStorage.removeItem('userId');
-  //   sessionStorage.removeItem('adminToken');
-  // }
+  if (logoutBtn.classList.contains('active')) {
+    sessionStorage.removeItem('loginToken');
+    sessionStorage.removeItem('userId');
+    sessionStorage.removeItem('adminToken');
+  }
 };
